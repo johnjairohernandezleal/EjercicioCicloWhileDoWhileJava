529e722caf607d61909f3cb962b28803aa625043
unknown <john_0112@hotmail.com>
2019-08-13 01:31:17 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        var i=0;
+        var j=0;
+        while (i<3){
+            System.out.println("i = " + i);
+            i++;
+        }
+
+
+        do{
+            System.out.println("j = " + j);
+            j++;
+        }while (j<3);
+    }
+    
+}
